9f1fd2d03cf6de468687e3a2d2a956a0c3eb9b02
demoinmuxi <kiteu@mails.ccnu.edu.cn>
2022-06-11 19:52:10 +0800

    n皇后

---
+++ n皇后/code.go
@@ -0,0 +1,57 @@
+package main
+
+import "fmt"
+
+func Queen(n int) *[][]int {
+	x, used, Xs := make([]int, n), make([]bool, n), [][]int{}
+	var backtrack func(int)
+	isXWorkable := func(cur int) bool {
+		for i := 0; i < cur; i++ {
+			for j := i + 1; j < cur; j++ {
+				if i+x[i] == j+x[j] || x[j]-x[i] == j-i {
+					return false
+				}
+			}
+		}
+		return true
+	}
+	backtrack = func(cur int) {
+		if !isXWorkable(cur) {
+			return
+		}
+		if cur == n {
+			X := make([]int, n)
+			copy(X, x)
+			Xs = append(Xs, X)
+			return
+		}
+		for i := 0; i < n; i++ {
+			if !used[i] {
+				used[i] = true
+				x[cur] = i
+				backtrack(cur + 1)
+				used[i] = false
+			}
+		}
+	}
+	backtrack(0)
+	return &Xs
+}
+func main() {
+	Xs := Queen(5)
+	fmt.Println(len(*Xs))
+	for _, v := range *Xs {
+		fmt.Println(v)
+	}
+	// 10
+	// [0 2 4 1 3]
+	// [0 3 1 4 2]
+	// [1 3 0 2 4]
+	// [1 4 2 0 3]
+	// [2 0 3 1 4]
+	// [2 4 1 3 0]
+	// [3 0 2 4 1]
+	// [3 1 4 2 0]
+	// [4 1 3 0 2]
+	// [4 2 0 3 1]
+}
